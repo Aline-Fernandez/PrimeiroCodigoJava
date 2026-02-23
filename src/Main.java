4e02b796ba7ca789cf22d6c9853ba08d93ad6a49
Aline Fernandes <fernandes.dev@outlook.com>
2026-02-23 13:56:48 -0300

    Adicionado exemplos de Tipos de dados

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public class Main{
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public static void main(String[] args){
+
+    //tipos de dados
+/*
+byte idade = 22, quantidadeDeDependentes = 1;
+short ano = 2000, mes = 5, dia = 15;
+int quantidade = , itens;
+long numeroDeVendas, colaboradores;
+float altura = 1.78f, largura = 0.65f, peso = 75.5f, aliquota = 0.38f;
+double preco = 1.99;
+boolean ativo, maior;
+char sexo;
+String nome;
+*/
+
+    byte idade = 22, quantidadeDeDependentes = 1;
+    short ano = 2000, mes = 5, dia = 15;
+    long numeroDeVendas, colaboradores;
+    float altura = 1.78f, largura = 0.65f, peso = 75.5f, aliquota = 0.38f;
+    double preco = 19.99;
     }
 }
